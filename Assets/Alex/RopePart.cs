f938407742d9b2b78e0bbdd3e7b94b651c2cfb31
Wardcore <40540898+Wardcore@users.noreply.github.com>
2019-02-01 11:39:53 +0100

    rope

--- Assets/Alex/RopePart.cs
+++ Assets/Alex/RopePart.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class RopePart : MonoBehaviour {
 
+	public int ID;
+	private Rope m_parent;
+	private LineRenderer lineTemp;
+	private bool cut = false;
+
+	void Start()
+	{
+		m_parent = transform.parent.GetComponent<Rope>();
+	}
+
+	void Update()
+	{
+		if(cut){
+			lineTemp.SetPositions(m_parent.SetLine(ID));
+		}
+	}
+
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.tag == "Scissors"){
-			print("ropePart");
+		if(other.tag == "Scissors" && !cut){
+			BreakLink();
 		}
 	}
 
 	public void BreakLink(){
-		transform.parent.GetComponent<Rope>().Desac();
+		//print("Cut " + gameObject.name);
+		//
+		lineTemp = gameObject.AddComponent<LineRenderer>();
+		lineTemp.positionCount = m_parent.m_childCount - ID;
+		cut = true;
+		//
 		Destroy(GetComponent<CharacterJoint>());
+		m_parent.Desac();
+		//
+
+		lineTemp.useWorldSpace = true;
+		lineTemp.startWidth = m_parent.m_lineSize;
+		lineTemp.endWidth = m_parent.m_lineSize;
+		lineTemp.material = m_parent.m_lineMat;
+
+		//lineTemp.SetPositions(m_parent.SetLine(ID));
 	}
 }
