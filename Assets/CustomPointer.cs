99bfc047757b2f68851049a316402e6cd66cc8a3
Wardcore <40540898+Wardcore@users.noreply.github.com>
2019-02-07 13:09:46 +0100

    hand detector ++

---
+++ Assets/CustomPointer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class CustomPointer : VRTK_Pointer 
+{
+    [Header("CUSTOM")]
+    public float m_timerMaxTime;
+
+    public float m_timer = 0;
+
+    public bool canTeleport = false;
+    public bool pointerActivated = false;
+
+    private VRTK_ControllerReference m_controller;
+    protected override void Update()
+    {
+        base.Update();
+
+        if((controllerEvents.GetTouchpadAxis().y >= 0.2f || controllerEvents.GetTouchpadAxis().y <= -0.2f) && pointerActivated && !canTeleport){
+            m_timer += Time.deltaTime;
+            if(!pointerActivated)
+                CustomActivatePointer();
+            //print("timer");
+        }
+        else if (m_timer != 0 && controllerEvents.GetTouchpadAxis().y < 0.2f && controllerEvents.GetTouchpadAxis().x < 0.2f && controllerEvents.GetTouchpadAxis().y > -0.2f && controllerEvents.GetTouchpadAxis().x > -0.2f){
+            m_timer = 0;
+            canTeleport = false;
+            if(pointerActivated){
+                pointerActivated = false;
+                CustomDeactivatePointer();
+            }
+            //print("Reset");
+        }
+
+        
+        if(pointerActivated && m_timer > m_timerMaxTime && !canTeleport){
+            m_timer = m_timerMaxTime;
+            canTeleport = true;
+            //print("timer end");
+        }
+    }
+
+
+    protected override void SubscribeActivationButton()
+    {
+        controllerEvents.SubscribeToAxisAliasEvent(SDK_BaseController.ButtonTypes.Touchpad, VRTK_ControllerEvents.AxisType.Axis, AxisTest);
+    }
+
+    protected override void UnsubscribeActivationButton()
+    {
+        controllerEvents.UnsubscribeToAxisAliasEvent(SDK_BaseController.ButtonTypes.Touchpad, VRTK_ControllerEvents.AxisType.Axis, AxisTest);
+    }
+
+    //
+    private void AxisTest(object sender, ControllerInteractionEventArgs e){
+        m_controller = e.controllerReference;
+        if(controllerEvents.GetTouchpadAxis().y >= 0.2f || controllerEvents.GetTouchpadAxis().y <= -0.2f){
+            pointerActivated = true;
+            CustomActivatePointer();
+        }
+        else if(controllerEvents.GetTouchpadAxis().y < 0.2f && controllerEvents.GetTouchpadAxis().x < 0.2f && controllerEvents.GetTouchpadAxis().y > -0.2f && controllerEvents.GetTouchpadAxis().x > -0.2f){
+            CustomDeactivatePointer();
+            pointerActivated = false;
+        }
+    }
+
+
+    //
+    private void CustomActivatePointer(){
+        controllerReference = m_controller;
+        OnActivationButtonPressed(controllerEvents.SetControllerEvent(ref activationButtonPressed, true));
+        PointerActivated();
+    }
+
+    private void CustomDeactivatePointer(){
+        controllerReference = m_controller;
+        PointerDeactivated();
+        OnActivationButtonReleased(controllerEvents.SetControllerEvent(ref activationButtonPressed, false));
+    }
+}
--- Assets/Paul/InputManager.cs
+++ Assets/Paul/InputManager.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 using VRTK;
 using UnityEngine.SceneManagement;
 
-public enum HandConfigs{
-	Rest = 0,
-	Point,
-	FingerGun,
-	ThumbUp,
-	Fist,
-	OKHand
-}
+// public enum HandConfigs{
+// 	Rest = 0,
+// 	Point,
+// 	FingerGun,
+// 	ThumbUp,
+// 	Fist,
+// 	OKHand
+// }
 public class InputManager : MonoBehaviour {
 
 	public VRTK_ControllerEvents m_rightEvent;
@@ -28,62 +28,68 @@ public class InputManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		m_leftConfig = SetConfig(m_leftEvent);
-		m_rightConfig = SetConfig(m_rightEvent);
+		//m_leftConfig = SetConfig(m_leftEvent);
+		//m_rightConfig = SetConfig(m_rightEvent);
 
 		if(m_leftEvent.buttonOnePressed && m_leftEvent.buttonTwoPressed && m_rightEvent.buttonOnePressed && m_rightEvent.buttonTwoPressed){
 			SceneManager.LoadScene("Showcase Alex", LoadSceneMode.Single);
 			SceneManager.LoadScene("Example_Interactions Alex", LoadSceneMode.Additive);
 		}
-		if(m_leftEvent.gripTouched || m_rightEvent.gripTouched){
+		/*if(m_leftEvent.gripTouched || m_rightEvent.gripTouched){
 			print("grip touched");
 		}
 		if(m_leftEvent.triggerTouched || m_rightEvent.triggerTouched){
 			print("trigger touched");
 		}
+		if( m_rightEvent.touchpadAxisChanged){
+			print("axis right : " + m_rightEvent.GetTouchpadAxisAngle());
+		}
+		if(m_leftEvent.touchpadAxisChanged){
+			print("axis left : " + m_leftEvent.GetTouchpadAxisAngle());
+		}*/
 	}
 
-	private HandConfigs SetConfig(VRTK_ControllerEvents InputEvent){
-		HandConfigs configs = HandConfigs.Rest;
+	// private HandConfigs SetConfig(VRTK_ControllerEvents InputEvent){
+	// 	HandConfigs configs = HandConfigs.Rest;
 
-		//REST
-		if(!InputEvent.gripPressed && !InputEvent.triggerTouched && !(InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
-			if(configs != HandConfigs.Rest){
-				configs = HandConfigs.Rest;
-				print("Rest");
-			}
-		}
-		//POINT
-		else if(InputEvent.gripPressed && !InputEvent.triggerTouched && (InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
-			if(configs != HandConfigs.Point){
-				configs = HandConfigs.Point;
-			}
-		}
-		//FINGER GUN
-		else if(InputEvent.gripPressed && !InputEvent.triggerTouched && !(InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
-			if(configs != HandConfigs.Point){
-				configs = HandConfigs.Point;
-			}
-		}
-		//THUMB UP
-		else if(InputEvent.gripPressed && InputEvent.triggerTouched && !(InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
-			if(configs != HandConfigs.ThumbUp){
-				configs = HandConfigs.ThumbUp;
-			}
-		}		
-		//FIST
-		else if(InputEvent.gripPressed && InputEvent.triggerTouched && (InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
-			if(configs != HandConfigs.Fist){
-				configs = HandConfigs.Fist;
-			}
-		}
-		//OK
-		else if(!InputEvent.gripPressed && InputEvent.triggerPressed && (InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
-			if(configs != HandConfigs.OKHand){
-				configs = HandConfigs.OKHand;
-			}
-		}
+	// 	//REST
+	// 	if(!InputEvent.gripPressed && !InputEvent.triggerTouched && !(InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
+	// 		if(configs != HandConfigs.Rest){
+	// 			configs = HandConfigs.Rest;
+	// 			print("Rest");
+	// 		}
+	// 	}
+	// 	//POINT
+	// 	else if(InputEvent.gripPressed && !InputEvent.triggerTouched && (InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
+	// 		if(configs != HandConfigs.Point){
+	// 			configs = HandConfigs.Point;
+	// 		}
+	// 	}
+	// 	//FINGER GUN
+	// 	else if(InputEvent.gripPressed && !InputEvent.triggerTouched && !(InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
+	// 		if(configs != HandConfigs.Point){
+	// 			configs = HandConfigs.Point;
+	// 		}
+	// 	}
+	// 	//THUMB UP
+	// 	else if(InputEvent.gripPressed && InputEvent.triggerTouched && !(InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
+	// 		if(configs != HandConfigs.ThumbUp){
+	// 			configs = HandConfigs.ThumbUp;
+	// 		}
+	// 	}		
+	// 	//FIST
+	// 	else if(InputEvent.gripPressed && InputEvent.triggerTouched && (InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
+	// 		if(configs != HandConfigs.Fist){
+	// 			configs = HandConfigs.Fist;
+	// 		}
+	// 	}
+	// 	//OK
+	// 	else if(!InputEvent.gripPressed && InputEvent.triggerPressed && (InputEvent.buttonOneTouched || InputEvent.buttonTwoTouched || InputEvent.touchpadTouched)){
+	// 		if(configs != HandConfigs.OKHand){
+	// 			configs = HandConfigs.OKHand;
+	// 		}
+	// 	}
 
-		return configs;
-	}
+	// 	return configs;
+	// }
 }
